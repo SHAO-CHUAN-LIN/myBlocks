551731980d63188183fccb82f9430e6372596cc6
SHAO-CHUAN-LIN <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-01-04 01:02:19 +0800

    Update blocks.js

--- blockly/blocks.js
+++ blockly/blocks.js
@@ -21,3 +21,15 @@ Blockly.Blocks['drone_locked'] = {
  this.setHelpUrl("");
   }
 };
+
+Blockly.Blocks['drone_take off'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("無人機")
+          .appendField(new Blockly.FieldDropdown([["起飛","ITEM1"], ["降落","ITEM2"]]), "dropdown");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+   this.setTooltip("");
+   this.setHelpUrl("");
+    }
