456be505d341215324937644d08afbee7ef9defc
SHAO-CHUAN-LIN <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-01-04 01:04:36 +0800

    Update javascript.js

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -1,3 +1,9 @@
+Blockly.Arduino['drone_take off'] = function(block) {
+  // TODO: Assemble Arduino into code variable.
+  var code = '...;\n';
+  return code;
+};
+
 Blockly.Arduino['drone_unlock'] = function(block) {
   // TODO: Assemble Arduino into code variable.
   var code = '...;\n';
