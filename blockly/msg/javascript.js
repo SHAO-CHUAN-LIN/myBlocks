de409d66cb7be584185fce34ac1578a45ebd479f
SHAO-CHUAN-LIN <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-01-03 17:16:46 +0800

    Add files via upload

---
+++ blockly/msg/javascript.js
@@ -0,0 +1,7 @@
+Blockly.Arduino['drone'] = function(block) {
+  var text_drone = block.getFieldValue('Drone');
+  var dropdown_dropdown = block.getFieldValue('dropdown');
+  // TODO: Assemble Arduino into code variable.
+  var code = '...;\n';
+  return code;
+};
