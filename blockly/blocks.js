902cce5b73ff7770fc29d91b4bd7037f51d1252d
SHAO-CHUAN-LIN <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-01-03 23:54:29 +0800

    Update blocks.js

--- blockly/blocks.js
+++ blockly/blocks.js
@@ -1,8 +1,19 @@
-Blockly.Blocks['drone'] = {
+Blockly.Blocks['drone_unlock'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("無人機")
-        .appendField(new Blockly.FieldDropdown([["起飛","ITEM1"], ["降落","ITEM2"]]), "dropdown");
+        .appendField("無人機解鎖");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['drone_locked'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("無人機上鎖");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(230);
