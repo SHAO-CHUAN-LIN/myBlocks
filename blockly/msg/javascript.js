40ec9fa8f833906b3e311453923081706128e04c
SHAO-CHUAN-LIN <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-01-03 17:17:40 +0800

    Delete javascript.js

--- blockly/msg/javascript.js
+++
@@ -1,7 +0,0 @@
-Blockly.Arduino['drone'] = function(block) {
-  var text_drone = block.getFieldValue('Drone');
-  var dropdown_dropdown = block.getFieldValue('dropdown');
-  // TODO: Assemble Arduino into code variable.
-  var code = '...;\n';
-  return code;
-};
