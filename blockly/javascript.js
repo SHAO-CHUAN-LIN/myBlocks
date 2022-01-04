1649593ce05bf655216aceec3db51cb8b78d6889
SHAO-CHUAN-LIN <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-01-04 22:15:50 +0800

    Update javascript.js

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -1,7 +1,7 @@
 Blockly.JavaScript['drone_command_method'] = function(block) {
-  var variable_bte = Blockly.Arduino.nameDB_.getName(block.getFieldValue('BTE'), Blockly.Variables.NAME_TYPE);
-  var value_name = Blockly.Arduino.valueToCode(block, 'NAME', Blockly.Arduino.ORDER_ATOMIC);
-  var variable_samplinginterval = Blockly.Arduino.nameDB_.getName(block.getFieldValue('samplingInterval'), Blockly.Variables.NAME_TYPE);
+  var variable_bte = Blockly.JavaScript.nameDB_.getName(block.getFieldValue('BTE'), Blockly.Variables.NAME_TYPE);
+  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
+  var variable_samplinginterval = Blockly.JavaScript.nameDB_.getName(block.getFieldValue('samplingInterval'), Blockly.Variables.NAME_TYPE);
   // TODO: Assemble Arduino into code variable.
   var code = '...;\n';
   return code;
