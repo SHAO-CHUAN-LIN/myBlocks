7beda3cee0022d00da3d2b864cf3bb542115dcbb
SHAO-CHUAN-LIN <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-01-04 16:45:52 +0800

    Update javascript.js

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -1,9 +1,37 @@
-Blockly.JavaScript['drone_command_method'] = function(block) {
-  var variable_bte = Blockly.JavaScript.nameDB_.getName(block.getFieldValue('BTE'), Blockly.Variables.NAME_TYPE);
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  var variable_samplinginterval = Blockly.JavaScript.nameDB_.getName(block.getFieldValue('samplingInterval'), Blockly.Variables.NAME_TYPE);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+Blockly.Arduino['drone_command_method'] = function(block) {
+  var variable_bte = Blockly.Arduino.nameDB_.getName(block.getFieldValue('BTE'), Blockly.Variables.NAME_TYPE);
+  var value_name = Blockly.Arduino.valueToCode(block, 'NAME', Blockly.Arduino.ORDER_ATOMIC);
+  var variable_samplinginterval = Blockly.Arduino.nameDB_.getName(block.getFieldValue('samplingInterval'), Blockly.Variables.NAME_TYPE);
+  // TODO: Assemble Arduino into code variable.
+  var jsbin = {
+    'root': 'https://bin.webduino.io',
+    'shareRoot': 'https://bin.webduino.io',
+    'runner': 'https://bin.webduino.io/runner',
+    'static': 'https://bin.webduino.io',
+    'version': '3.38.13',
+    user: {},
+  };
+  
+  (function () {
+    if (jsbin.user && jsbin.user.name) {
+      $('.loggedout').hide();
+      var menu = $('.loggedin').show();
+      var html = $('#profile-template').text();
+      var $html = $(html.replace(/({.*?})/g, function (all, match) {
+        var key = match.slice(1, -1).trim(); // ditch the wrappers
+        return jsbin.user[key] || '';
+      }));
+      if (jsbin.user.pro) {
+        document.documentElement.className += ' pro';
+        $html.find('.gopro').remove();
+      } else {
+        $html.find('.pro').remove();
+      }
+      menu.append($html);
+    } else {
+      $('.loggedout').show();
+    }
+  })();
   return code;
 };
 
