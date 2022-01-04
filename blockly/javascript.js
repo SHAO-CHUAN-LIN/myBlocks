0d99b56d904d4f57d910c8e75e63a86120721d0b
SHAO-CHUAN-LIN <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-01-04 16:52:17 +0800

    Update javascript.js

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -3,35 +3,7 @@ Blockly.Arduino['drone_command_method'] = function(block) {
   var value_name = Blockly.Arduino.valueToCode(block, 'NAME', Blockly.Arduino.ORDER_ATOMIC);
   var variable_samplinginterval = Blockly.Arduino.nameDB_.getName(block.getFieldValue('samplingInterval'), Blockly.Variables.NAME_TYPE);
   // TODO: Assemble Arduino into code variable.
-  var jsbin = {
-    'root': 'https://bin.webduino.io',
-    'shareRoot': 'https://bin.webduino.io',
-    'runner': 'https://bin.webduino.io/runner',
-    'static': 'https://bin.webduino.io',
-    'version': '3.38.13',
-    user: {},
-  };
-  
-  (function () {
-    if (jsbin.user && jsbin.user.name) {
-      $('.loggedout').hide();
-      var menu = $('.loggedin').show();
-      var html = $('#profile-template').text();
-      var $html = $(html.replace(/({.*?})/g, function (all, match) {
-        var key = match.slice(1, -1).trim(); // ditch the wrappers
-        return jsbin.user[key] || '';
-      }));
-      if (jsbin.user.pro) {
-        document.documentElement.className += ' pro';
-        $html.find('.gopro').remove();
-      } else {
-        $html.find('.pro').remove();
-      }
-      menu.append($html);
-    } else {
-      $('.loggedout').show();
-    }
-  })();
+  var code = '...;\n';
   return code;
 };
 
