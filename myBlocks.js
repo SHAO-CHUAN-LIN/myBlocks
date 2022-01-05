f979f60adbce645187d79c1d14f5c672c6fa6ca0
SHAO-CHUAN-LIN <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-01-05 17:39:47 +0800

    Update myBlocks.js

--- myBlocks.js
+++ myBlocks.js
@@ -3,6 +3,19 @@
   'use strict';
 
   function test() {
+    let device = await navigator.bluetooth.requestDevice(
+      {
+        filters:
+        [ 
+            { namePrefix: 'HC' } 
+        ],
+        optionalServices: [ 0xff0f ]
+      });
+    let server = await device.gatt.connect();
+    let service = await server.getPrimaryService(0xff0f);
+    let characteristic = await service.getCharacteristic(0xfffc);
+    
+    characteristic.writeValue(parseInt('24 4D 3C 10 C8 DC 05 DC 05 D0 07 E8 03 DC 05 DC 05 DC 05 DC 05 E4',16) );
     
   }
 
