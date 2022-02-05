7bf69913efd3c0214fc1979155752d15d9900fab
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-06 04:33:26 +0800

    Update WebSerial.js

--- WebSerial.js
+++ WebSerial.js
@@ -1,51 +1,47 @@
-document.write('<input type="button" id="gamebutton_webserial_open" style="z-index:999" value="Select Port">');
+document.write('<input type="button" id="gamebutton_webserial_open" style="z-index:999" value="選擇序列埠">');
 
-let serial_buttonRequest = document.getElementById('gamebutton_webserial_open');
+let serial_buttonRequest = document.getElementById('gamebutton_webserial_open'); //儲存button回傳的值
 
 
 serial_buttonRequest.addEventListener('click', async () => {startSerial();});
 
 let stopFlag = false;
-        async function startSerial() {
+async function startSerial() {
+    try {
+        console.log("INFO: Start to connect...");
+        stopFlag = false;
+        const port = await navigator.serial.requestPort();
+        await port.open({ baudRate: 115200 }); //wait baudrate data,or jump to catch
+        while (port.readable) {
+            const reader = port.readable.getReader();
             try {
-                console.log("INFO: 接続が確立しました");
-                stopFlag = false;
-                const port = await navigator.serial.requestPort();
-                // const port =  navigator.serial.requestPort();
-                await port.open({ baudRate: 115200 }); //wait baudrate data,or tun to catch
-                while (port.readable) {
-                    const reader = port.readable.getReader();
-                    try {
-                        while (!stopFlag) {
-                            const { value, done } = await reader.read();
-                            if (done) {
-                                console.log("INFO: 読込モード終了");
-                                break;
-                            }
-                            //👇生データはバイナリなので、ユニコード文字へデコード
-                            const inputValue = new TextDecoder().decode(value);
-                            console.log(inputValue);
-                        }
-                    } catch (error) {
-                        console.log("ERROR: 読み出し失敗");
-                        console.log(error);
-                    } finally {
-                        reader.releaseLock();
-                        await port.close();
-                        console.log("INFO: 接続を切断しました");
+                while (!stopFlag) {
+                    const { value, done } = await reader.read();
+                    if (done) {
+                        console.log("INFO: 読込モード終了");
+                        break;
                     }
+                    //👇生データはバイナリなので、ユニコード文字へデコード
+                    const inputValue = new TextDecoder().decode(value);
+                    console.log(inputValue);
                 }
             } catch (error) {
-                console.log("ERRORR:ポートが開けません");
-                console.log(error); //DOMException: Failed to open serial port.
+                console.log("ERROR: 読み出し失敗");
+                console.log(error);
+            } finally {
+                reader.releaseLock();
+                await port.close();
+                console.log("INFO: 接続を切断しました");
             }
         }
-        function stopSerial() {
-            stopFlag = true;
-            // reader.releaseLock();
-            // await port.close();
-            // console.log("INFO: 接続を切断しました");
-        }
+    } catch (error) {
+        console.log("ERRORR:Port is not open");
+        console.log(error); //DOMException: Failed to open serial port.
+    }
+}
+function stopSerial() {
+    stopFlag = true;
+}
 
 function serial_message(msg) {
 	serial_status.innerText = msg;
