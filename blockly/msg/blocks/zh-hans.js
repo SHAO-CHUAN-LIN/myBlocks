5cfe59a793f2f1a661cfa70e653a391dcee2be99
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-05 23:53:41 +0800

    Update zh-hans.js

--- blockly/msg/blocks/zh-hans.js
+++ blockly/msg/blocks/zh-hans.js
@@ -1,3 +1,3 @@
+Blockly.Msg.WEBSERIAL_GETID_SHOW = "Web Serial 取得按钮ID";
 Blockly.Msg.WEBSERIAL_CONNECT_SHOW = "选择序列埠按钮";
 Blockly.Msg.WEBSERIAL_CLOSE_SHOW = "关闭序列埠按钮";
-Blockly.Msg.WEBSERIAL_GETID_SHOW = "Web Serial 取得按钮ID";
