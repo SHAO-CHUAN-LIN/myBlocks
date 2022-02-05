8b193fca876293a9cb09c4437ff1eb2960d38a03
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-06 05:09:43 +0800

    Update WebSerial.js

--- WebSerial.js
+++ WebSerial.js
@@ -1,7 +1,9 @@
-document.write('<input type="button" id="gamebutton_webserial_open" style="z-index:999" value="選擇序列埠">');
+document.write('<input type="button" id="Button_webserial_open" style="z-index:999" value="選擇序列埠">');
+document.write('<input type="button" id="Button_webserial_close" style="z-index:999" value="關閉序列埠">');
 
-let serial_buttonRequest = document.getElementById('gamebutton_webserial_open'); //儲存button回傳的值
 
+let serial_buttonRequest = document.getElementById('Button_webserial_open'); //儲存button回傳的值
+let serial_buttonClose = document.getElementById('Button_webserial_close');
 
 serial_buttonRequest.addEventListener('click', async () => {startSerial();});
 
@@ -39,9 +41,7 @@ async function startSerial() {
         console.log(error); //DOMException: Failed to open serial port.
     }
 }
-function stopSerial() {
-    stopFlag = true;
-}
+
 
 function serial_message(msg) {
 	serial_status.innerText = msg;
