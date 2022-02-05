6f421f7b830b46a0a09c55295daa00784ee3b79c
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-06 05:28:06 +0800

    Update WebSerial.js

--- WebSerial.js
+++ WebSerial.js
@@ -7,37 +7,13 @@ let serial_buttonClose = document.getElementById('Button_webserial_close');
 
 serial_buttonRequest.addEventListener('click', async () => {startSerial();});
 
-let stopFlag = false;
 async function startSerial() {
-    try {
+    try{
         console.log("INFO: Start to connect...");
-        stopFlag = false;
         const port = await navigator.serial.requestPort();
         await port.open({ baudRate: 115200 }); //wait baudrate data,or jump to catch
-//         while (port.readable) {
-//             const reader = port.readable.getReader();
-//             try {
-//                 while (!stopFlag) {
-//                     const { value, done } = await reader.read();
-//                     if (done) {
-//                         console.log("INFO: 読込モード終了");
-//                         break;
-//                     }
-//                     //👇生データはバイナリなので、ユニコード文字へデコード
-//                     const inputValue = new TextDecoder().decode(value);
-//                     console.log(inputValue);
-//                 }
-//             } catch (error) {
-//                 console.log("ERROR: 読み出し失敗");
-//                 console.log(error);
-//             } finally {
-//                 reader.releaseLock();
-//                 await port.close();
-//                 console.log("INFO: 接続を切断しました");
-//             }
-//         }
     }
-	catch (error) {
+    catch(error){
         console.log("ERRORR:Port is not open");
         console.log(error); //DOMException: Failed to open serial port.
     }
@@ -47,3 +23,4 @@ async function startSerial() {
 function serial_message(msg) {
 	serial_status.innerText = msg;
 }
+
