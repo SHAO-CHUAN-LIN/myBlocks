83d8918b59ca5b17b4f5454c2083b05284f39fa3
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-06 01:43:09 +0800

    Update myBlocks.js

--- myBlocks.js
+++ myBlocks.js
@@ -2,10 +2,15 @@
 
   'use strict';
 
-  function OpenPort() {
-
-    }
-
-  window.OpenPort = OpenPort;
+	function webserial_button_onclick(input_button, input_id) {
+		if (document.getElementById(input_id)) {
+			if (input_button=="open")
+				document.getElementById(input_id).addEventListener('click', async () => {buttonRequest();});
+			else
+				document.getElementById(input_id).addEventListener('click', async () => {buttonClose();});	
+		}
+	}
+  
+	window.webserial_button_onclick = webserial_button_onclick;
 
 }(window, window.document));
