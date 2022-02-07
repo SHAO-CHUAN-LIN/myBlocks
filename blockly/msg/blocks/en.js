6c5638d0436dc38725962ab1a345d78b1b8c56af
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-07 17:58:36 +0800

    Update en.js

--- blockly/msg/blocks/en.js
+++ blockly/msg/blocks/en.js
@@ -1,3 +1,6 @@
 Blockly.Msg.WEBSERIAL_COMPORT_SELECT = "Get Web Serial information";
 Blockly.Msg.WEBSERIAL_CONNECT_SHOW = "Select port";
 Blockly.Msg.WEBSERIAL_CLOSE_SHOW = "Close port";
+Blockly.Msg.ROBOFLY_SHOW = "ROBOFLY";
+Blockly.Msg.WEBSERIAL_UNLOCK_SHOW = "unlock";
+Blockly.Msg.WEBSERIAL_LOCK_SHOW = "lock";
