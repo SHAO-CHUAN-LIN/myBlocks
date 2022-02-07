0414ca37d3e2a9b2e89a35985bccf175e9e060c9
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-07 13:20:48 +0800

    Update myBlocks.js

--- myBlocks.js
+++ myBlocks.js
@@ -1,6 +1,12 @@
 +(function (window, document) {
 
   'use strict';
+	let serial_buttonRequest = document.getElementById('Button_webserial_open');
+	let serial_buttonClose = document.getElementById('Button_webserial_close');
+	
+	function webserial_button(input_id, input_show) {
+		document.getElementById(input_id).style.display = input_show;
+	}
 
 	function webserial_button_onclick(input_button, input_id) {
 		if (document.getElementById(input_id)) {
