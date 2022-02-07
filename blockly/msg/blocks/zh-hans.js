a8f0a43b007304e49b2d56b0ee7a42521030c4e2
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-07 17:59:39 +0800

    Update zh-hans.js

--- blockly/msg/blocks/zh-hans.js
+++ blockly/msg/blocks/zh-hans.js
@@ -1,3 +1,6 @@
 Blockly.Msg.WEBSERIAL_COMPORT_SELECT = "Web Serial 取得序列埠讯息";
 Blockly.Msg.WEBSERIAL_CONNECT_SHOW = "选择序列埠";
 Blockly.Msg.WEBSERIAL_CLOSE_SHOW = "关闭序列埠";
+Blockly.Msg.ROBOFLY_SHOW = "ROBOFLY";
+Blockly.Msg.WEBSERIAL_UNLOCK_SHOW = "解锁";
+Blockly.Msg.WEBSERIAL_LOCK_SHOW = "上锁";
