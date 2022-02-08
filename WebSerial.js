8c837f7f3932a2f0c1f0ba128eeeb5b486a0c52d
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-08 11:03:35 +0800

    Update WebSerial.js

--- WebSerial.js
+++ WebSerial.js
@@ -1,4 +1,4 @@
-document.write('<input type="button" id="Button_webserial_open" style="display:none;z-index:999" value="選擇序列埠">');
+document.write('<input type="button" id="Button_webserial_open" style="z-index:999" value="選擇序列埠">');
 
 
 let serial_buttonRequest = document.getElementById('Button_webserial_open'); //儲存button回傳的值
