6fff3e8d9ae1139087f6e8ddf9bedad8cedb2c79
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-09 22:32:44 +0800

    Update myBlocks.js

--- myBlocks.js
+++ myBlocks.js
@@ -8,6 +8,7 @@
 // 		if (document.getElementById(input_serialportstatus)) {
 		var serialport_status = document.getElementById(input_serialportstatus);
 			if (serialport_status == "open")
+				document.getElementById(input_serialportstatus).style.display = block;
 				document.getElementById(input_serialportstatus).addEventListener('click', async () => {startSerial();});
 // 		}
 	}
