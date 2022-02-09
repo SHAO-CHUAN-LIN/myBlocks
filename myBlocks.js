ff0f5f7c9f034ef9774029f3496e4cccc06def8e
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-10 03:01:35 +0800

    Update myBlocks.js

--- myBlocks.js
+++ myBlocks.js
@@ -5,15 +5,15 @@
   let serial_buttonRequest = document.getElementById('button_webserial_open');
 
 	
-//   function webserial_get_serialport(input_status, input_select) {
-// 	  document.getElementById(input_status).style.display = input_select;
-// 	}
   function webserial_get_serialport(input_status, input_select) {
-	  if (document.getElementById(input_status)) {
-			if (input_status=="open")
-				document.getElementById(input_id).addEventListener('click', async () => {startSerial();});	
-		}
+	  document.getElementById(input_status).style.display = input_select;
 	}
+//   function webserial_get_serialport(input_status, input_select) {
+// 	  if (document.getElementById(input_status)) {
+// 			if (input_status=="open")
+// 				document.getElementById(input_id).addEventListener('click', async () => {startSerial();});	
+// 		}
+// 	}
   
 	window.webserial_get_serialport = webserial_get_serialport;
 
