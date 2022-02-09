674c6e04f7042ac1a3be62179c9c58670dea6700
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-09 23:43:25 +0800

    Update myBlocks.js

--- myBlocks.js
+++ myBlocks.js
@@ -1,8 +1,17 @@
 +(function (window, document) {
 
   'use strict';
-  let serial_uint8 = document.getElementById('serial_uint8');
-  let serial_sendUint8 = document.getElementById('serial_sendUint8');
+	let serial_baud = document.getElementById('serial_baud');
+	let serial_text = document.getElementById('serial_text');
+	let serial_uint8 = document.getElementById('serial_uint8');
+	let serial_data = document.getElementById('serial_data');
+	let serial_status = document.getElementById('serial_status');
+	let serial_state = document.getElementById('serial_state');
+	let serial_buttonRequest = document.getElementById('button_webserial_open');
+	let serial_sendText = document.getElementById('serial_sendText');
+	let serial_sendUint8 = document.getElementById('serial_sendUint8');
+	let serial_clearText = document.getElementById('serial_clearText');	
+	let serial_end = document.getElementById('serial_end');
 	
 	function webserial_get_serialport(input_id, input_show) {
 // 		if (document.getElementById(input_serialportstatus)) {
