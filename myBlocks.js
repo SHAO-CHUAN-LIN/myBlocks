205ecbc5536e73d4fdd95f52c8be52964c35b556
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-09 21:33:15 +0800

    Update myBlocks.js

--- myBlocks.js
+++ myBlocks.js
@@ -1,12 +1,19 @@
 +(function (window, document) {
 
   'use strict';
+  let serial_uint8 = document.getElementById('serial_uint8');
+  let serial_sendUint8 = document.getElementById('serial_sendUint8');
 	
 	function webserial_get_serialport(input_button, input_id) {
 		if (document.getElementById(input_id)) {
 			if (input_button=="open")
 				document.getElementById(input_id).addEventListener('click', async () => {startSerial();});
 		}
+	}
+	
+	function robofly_unlock_command(input_cmd) {
+		serial_uint8.value = input_cmd;
+		serial_sendUint8.click();
 	}
   
 	window.webserial_get_serialport = webserial_get_serialport;
