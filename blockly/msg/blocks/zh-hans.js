3605817c5819d4d812a743abeabcc2baf4dac958
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-09 23:13:57 +0800

    Update zh-hans.js

--- blockly/msg/blocks/zh-hans.js
+++ blockly/msg/blocks/zh-hans.js
@@ -4,3 +4,4 @@ Blockly.Msg.WEBSERIAL_CLOSE_SHOW = "关闭序列埠";
 Blockly.Msg.ROBOFLY_SHOW = "ROBOFLY";
 Blockly.Msg.WEBSERIAL_UNLOCK_SHOW = "无人机解锁";
 Blockly.Msg.WEBSERIAL_LOCK_SHOW = "无人机上锁";
+Blockly.Msg.WEBSERIAL_SHOW_SHOW = "显示";
