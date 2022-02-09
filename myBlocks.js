1feaf0a974b198dea775e327c4ed38a976b34aeb
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-10 02:56:26 +0800

    Update myBlocks.js

--- myBlocks.js
+++ myBlocks.js
@@ -5,15 +5,13 @@
   let serial_buttonRequest = document.getElementById('button_webserial_open');
 
 	
-  function webserial_get_serialport(input_status, input_select) {
-	  document.getElementById(input_status).style.display = input_select;
-	}
+//   function webserial_get_serialport(input_status, input_select) {
+// 	  document.getElementById(input_status).style.display = input_select;
+// 	}
   function webserial_get_serialport(input_status, input_select) {
 	  if (document.getElementById(input_status)) {
 			if (input_status=="open")
-				document.getElementById(input_id).addEventListener('click', async () => {startSerial();});
-			else
-				document.getElementById(input_id).addEventListener('click', async () => {buttonClose();});	
+				document.getElementById(input_id).addEventListener('click', async () => {startSerial();});	
 		}
 	}
   
