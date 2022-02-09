8a525de4e2f0637f1a796b1d77ccd4d812a08d51
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-09 23:17:39 +0800

    Update en.js

--- blockly/msg/blocks/en.js
+++ blockly/msg/blocks/en.js
@@ -1,6 +1,6 @@
 Blockly.Msg.WEBSERIAL_COMPORT_SELECT = "Get Web Serial information";
-Blockly.Msg.WEBSERIAL_CONNECT_SHOW = "Select port";
-Blockly.Msg.WEBSERIAL_CLOSE_SHOW = "Close port";
+Blockly.Msg.WEBSERIAL_BUTTON_CONNECT_SHOW = "Select port";
+Blockly.Msg.WEBSERIAL_BUTTON_CLOSE_SHOW = "Close port";
 Blockly.Msg.ROBOFLY_SHOW = "ROBOFLY";
 Blockly.Msg.WEBSERIAL_UNLOCK_SHOW = "drone unlock";
 Blockly.Msg.WEBSERIAL_LOCK_SHOW = "drone lock";
