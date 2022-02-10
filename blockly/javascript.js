fd653f2d0c90d3ed1e79a642e6a8712dfb9f1d24
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-10 20:44:50 +0800

    Update javascript.js

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -8,7 +8,7 @@
 // };
 Blockly.JavaScript['webserial_get_serialport'] = function(block) {
   var value_status = block.getFieldValue('status_');   
-  var value_select = Blockly.JavaScript.valueToCode(block, 'select_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_select = block.getFieldValue('select_');
   var code = 'webserial_port("'+value_status+'","'+value_select+'")';
   return code;
 };
