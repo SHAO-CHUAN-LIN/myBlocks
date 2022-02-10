58204d2004fd1a74d0f575f2a49c43624ed02c92
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-10 11:57:04 +0800

    Update myBlocks.js

--- myBlocks.js
+++ myBlocks.js
@@ -6,7 +6,7 @@
 
 	
   function webserial_get_serialport(input_status, input_select) {
-	  document.getElementById(input_status).style.display = input_select;
+	  document.getElementById(input_status).style.display = "";
 	}
 //   function webserial_get_serialport(input_status, input_select) {
 // 	  if (document.getElementById(input_status)) {
