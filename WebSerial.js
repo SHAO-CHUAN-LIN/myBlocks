a959d75d939c31b0b1b636616bd1fddad5c7b45a
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-11 16:55:36 +0800

    Update WebSerial.js

--- WebSerial.js
+++ WebSerial.js
@@ -13,6 +13,7 @@
 	
   function webserial_port(input_status) {
 // 	  if (document.getElementById(input_status)) {
+	  let status_value = 
 		  if (input_status == "open")
 			  serial_buttonRequest.style.display = "block";
 		  else if(input_status == "close")
