f33aa5039bddccf04e4d281a33268ceda16ffacc
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-11 14:10:56 +0800

    Update myBlocks.js

--- myBlocks.js
+++ myBlocks.js
@@ -2,21 +2,28 @@
 
   'use strict';
 	
-//   let serial_buttonRequest = document.getElementById('button_webserial_open');
-//   let serial_buttonClose = document.getElementById('button_webserial_close');
+  let serial_buttonRequest = document.getElementById('button_webserial_open');
+  let serial_buttonClose = document.getElementById('button_webserial_close');
 
 	
-  function webserial_port(input_status, input_select) {
+  function webserial_get_port(input_status, input_select) {
 	  document.getElementById(input_status).style.display = input_select;
 // 	  serial_buttonRequest.style.display = input_select;
 	}
-//   function webserial_port(input_status, input_select) {
-// 	  if (document.getElementById(input_status)) {
-// 			if (input_status=="open")
-// 				document.getElementById(input_id).addEventListener('click', async () => {startSerial();});	
-// 		}
-// 	}
-  
+	
+  function webserial_port(input_status) {
+	  if (document.getElementById(input_status)) {
+		  if (input_status=="open")
+			  serial_buttonRequest.style.display = "block";
+		  else if(input_status=="close")
+			  serial_buttonRequest.style.display = "none";
+		  else
+			  return "";
+// 		  document.getElementById(input_id).addEventListener('click', async () => {startSerial();});	
+		}
+	}
+	
+	window.webserial_get_port = webserial_get_port;
 	window.webserial_port = webserial_port;
 
 }(window, window.document));
