fca5d5267ed98c01fcead5ac81e72100a4ad11da
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-18 20:25:49 +0800

    Update WebSerial.js

--- WebSerial.js
+++ WebSerial.js
@@ -2,10 +2,6 @@
 
   'use strict';
 	
-  var serial_buttonRequest = document.getElementById('button_webserial_open');
-  let serial_buttonClose = document.getElementById('button_webserial_close');
-
-	
   function webserial_get_port(input_status, input_select) {
 	  document.getElementById(input_status).style.display = input_select;
 // 	  serial_buttonRequest.style.display = input_select;
