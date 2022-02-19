59ece05ad376e1c76605c283c2824d8503aa8ed0
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-19 15:22:53 +0800

    Update javascript.js

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -6,16 +6,16 @@ Blockly.JavaScript['webserial_get_serialport'] = function(block) {
 };
 
 Blockly.JavaScript['webserial_serialport'] = function(block) {
-  var value_status = block.getFieldValue('status');   
-  var code = 'webserial_port("'+value_status+'");\n';
+    var value_status = block.getFieldValue('status');   
+    var code = 'webserial_port("'+value_status+'");\n';
   return code;
 };
 
 Blockly.JavaScript['webserial_open'] = function(block) {
-   var hidden_dropdown = document.getElementById('demo-select').style.display = "none";
-//     var code = 'webserial_port("open");\n' + 'document.getElementById("demo-select");\n'
-    
-    var code = 'webserial_port("open");\n'+'document.getElementById("demo-area-01-show").innerHTML = "等待連線...";\n'+ hidden_dropdown +';\n';
+    var hidden_dropdown = document.getElementById('demo-select').style.display = "none";  
+    var code = 'webserial_port("open");\n'
+                +'document.getElementById("demo-area-01-show").innerHTML = "等待連線...";\n'+
+                'dropdown_box：'+ hidden_dropdown +';\n';
   return code;
 };
 
@@ -30,13 +30,13 @@ Blockly.JavaScript['webserial_status'] = function(block) {
 };
 
 Blockly.JavaScript['robofly_unlock_command'] = function(block) {
-  var value_lock_status = block.getFieldValue('lock_status');
-  var value_time = Blockly.JavaScript.valueToCode(block, 'delay_time', Blockly.JavaScript.ORDER_ATOMIC);
-  if(value_lock_status == "unlock")
+    var value_lock_status = block.getFieldValue('lock_status');
+    var value_time = Blockly.JavaScript.valueToCode(block, 'delay_time', Blockly.JavaScript.ORDER_ATOMIC);
+    if(value_lock_status == "unlock")
     var code = 'send_data("0x24,0x4d,0x3c,0x10,0xc8,0xdc,0x05,0xdc,0x05,0xd0,0x07,0xe8,0x03,0xdc,0x05,0xdc,0x05,0xdc,0x05,0xdc,0x05,0xe4");\n'+
                'await delay('+ value_time +');\n';
-  else if(value_lock_status == "lock")
-    var code = 'send_data("0x24,0x4d,0x3c,0x10,0xc8,0xdc,0x05,0xdc,0x05,0xe8,0x03,0xe8,0x03,0xdc,0x05,0xdc,0x05,0xdc,0x05,0xdc,0x05,0xd8");\n'+
+    else if(value_lock_status == "lock")
+      var code = 'send_data("0x24,0x4d,0x3c,0x10,0xc8,0xdc,0x05,0xdc,0x05,0xe8,0x03,0xe8,0x03,0xdc,0x05,0xdc,0x05,0xdc,0x05,0xdc,0x05,0xd8");\n'+
                'await delay('+ value_time +');\n';
 //   var code = "robotfly_status('"+ value_lock_status +"')";
 //   return [code, Blockly.JavaScript.ORDER_NONE];
