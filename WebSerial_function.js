c9233e5850ff517fc93d49675a17dedccb671dc4
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-02-21 14:58:06 +0800

    Update WebSerial_function.js

--- WebSerial_function.js
+++ WebSerial_function.js
@@ -35,7 +35,7 @@ async function startSerial() {
 	    console.log(port_info);
 	    console.log(info);
 	    if(port_info){
-		    console.log("連線成功");
+// 		    console.log("連線成功");
 		    document.getElementById('button_webserial_open').style.display = "none";
 		    document.getElementById('demo-area-01-show').innerHTML = "連線成功！";
 		    document.getElementById('serial_text').value = "連線成功！";
