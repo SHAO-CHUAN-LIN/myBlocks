ada0fc63bfde14fbf46c3dd2541bb783ba8d0245
林劭全 <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-03-04 11:10:44 +0800

    Update zh-hant.js

--- blockly/msg/blocks/zh-hant.js
+++ blockly/msg/blocks/zh-hant.js
@@ -4,6 +4,7 @@ Blockly.Msg.WEBSERIAL_BUTTON_CONNECT_SHOW = "開啟";
 Blockly.Msg.WEBSERIAL_BUTTON_CLOSE_SHOW = "關閉";
 Blockly.Msg.WEBSERIAL_OPEN = "Web Serial 開啟序列埠";
 Blockly.Msg.WEBSERIAL_CLOSE = "Web Serial 關閉序列埠";
+Blockly.Msg.WEBSERIAL_GETSTATE_SHOW = "取得連線狀態";
 Blockly.Msg.ROBOFLY_SHOW = "ROBOFLY";
 Blockly.Msg.ROBOFLY_UNLOCK_SHOW = "無人機解鎖";
 Blockly.Msg.ROBOFLY_LOCK_SHOW = "無人機上鎖";
